ce7438b69d6f9bd6c8efaf50dea686d4e5d8f76d
Walter A. Boring IV <waboring@hemna.com>
2018-10-17 21:46:50 +0000

    Saving of the auto filled in data works.
    This patch fixes an issue where the data that was populated
    by the yaml parsing capability wasn't being saved to the DB.

--- crowbar_framework/app/assets/javascripts/barclamps/ses/application.js
+++ crowbar_framework/app/assets/javascripts/barclamps/ses/application.js
@@ -18,39 +18,59 @@
 $(document).ready(function($) {
 
   $( "#parse_yaml" ).click(function() {
-    console.log("SHITBALLS BUTTON PRESSED");
     var raw_yaml = $("#yaml_file").val().trim();
-    console.log(raw_yaml);
-
     var ses_config = YAML.parse(raw_yaml);
-    console.log("shitballs", ses_config);
-
 
     /* Now that we have the yaml parsed */
-
     console.log(ses_config.ceph_conf)
-    console.log(ses_config.ceph_conf.cinder-backup)
     $("#ceph_conf_cluster_network").val(ses_config.ceph_conf.cluster_network);
+    $("#ceph_conf_cluster_network").trigger('change');
+
     $("#ceph_conf_fsid").val(ses_config.ceph_conf.fsid);
+    $("#ceph_conf_fsid").trigger('change');
+
     $("#ceph_conf_mon_host").val(ses_config.ceph_conf.mon_host);
+    $("#ceph_conf_mon_host").trigger('change');
+
     $("#ceph_conf_mon_initial_members").val(ses_config.ceph_conf.mon_initial_members);
+    $("#ceph_conf_mon_initial_members").trigger('change');
+
     $("#ceph_conf_public_network").val(ses_config.ceph_conf.public_network);
+    $("#ceph_conf_public_network").trigger('change');
 
     $("#cinder_key").val(ses_config.cinder.key);
+    $("#cinder_key").trigger('change');
+
     $("#cinder_rbd_store_pool").val(ses_config.cinder.rbd_store_pool);
+    $("#cinder_rbd_store_pool").trigger('change');
+
     $("#cinder_rbd_store_user").val(ses_config.cinder.rbd_store_user);
+    $("#cinder_rbd_store_user").trigger('change');
 
     $("#cinder_backup_key").val(ses_config["cinder-backup"].key);
+    $("#cinder_backup_key").trigger('change');
+
     $("#cinder_backup_rbd_store_pool").val(ses_config["cinder-backup"].rbd_store_pool);
+    $("#cinder_backup_rbd_store_pool").trigger('change');
+
     $("#cinder_backup_rbd_store_user").val(ses_config["cinder-backup"].rbd_store_user);
+    $("#cinder_backup_rbd_store_user").trigger('change');
+
 
     $("#glance_key").val(ses_config.glance.key);
+    $("#glance_key").trigger('change');
+
     $("#glance_rbd_store_pool").val(ses_config.glance.rbd_store_pool);
+    $("#glance_rbd_store_pool").trigger('change');
+
     $("#glance_rbd_store_user").val(ses_config.glance.rbd_store_user);
+    $("#glance_rbd_store_user").trigger('change');
 
     $("#nova_rbd_store_pool").val(ses_config.nova.rbd_store_pool);
+    $("#nova_rbd_store_pool").trigger('change');
 
     $("#radosgw_urls").val(ses_config.radosgw_urls);
+    $("#radosgw_urls").trigger('change');
 
   });
 
